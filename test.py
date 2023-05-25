4a1a04363a6442842c1f75f1a19e0250fcf0fd0f
karosis88 <kar.petrosyanpy@gmail.com>
2023-05-25 14:40:08 +0300

    Remove template.yml

--- test.py
+++ test.py
@@ -10,6 +10,7 @@ def network_filter(packet: NetworkPacket):
     if isinstance(packet, IPv4Packet):
         return packet.src_addr == "10.0.2.15"
     return False
+
 f = Filter(name="test", network_filter=network_filter)
 
 handler = Handler(name="test", filters=[f], callback=callback)
